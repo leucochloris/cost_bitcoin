22ddefe7598249ac2cf2bc48eab04a9c26904e82
Alexandr Shesterikov <108606736+leucochloris@users.noreply.github.com>
2022-07-10 14:07:38 +0300

    Draw a graphic of cost BTC (lesson from https://www.youtube.com/watch?v=JQUTXXyPel4&list=PL4sJ_PtqW49DPwy4J_kTWCYcV3Gplyeea&index=3

--- another_visual.py
+++ another_visual.py
@@ -1,8 +1,25 @@
 import matplotlib.pyplot as plt
+import json
+from datetime import datetime
+
+def get_data():
+    with open('market-price.json') as file:
+        data = json.load(file)
+        return data
+
+def data_plot():
+    data = get_data()
+    x_list = []
+    y_list = []
+    for i in data['values']:
+        x_list.append(datetime.utcfromtimestamp(i['x']))
+        y_list.append(i['y'])
+    return x_list, y_list
 
 plt.title ('Graphic of cost Bitcoin', fontsize=13, fontweight='bold', color = 'blue')
 plt.xlabel ('Period', fontsize=10, color='black')
 plt.ylabel ('Cost', fontsize=10, color='black')
+plt.plot(*data_plot(), label='Market cost')
 plt.legend()
 plt.grid()
 plt.show()
