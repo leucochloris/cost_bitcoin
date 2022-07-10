9e2867c11e218f26f0444152788b20bfb323b37f
Alexandr Shesterikov <108606736+leucochloris@users.noreply.github.com>
2022-07-10 13:46:24 +0300

    Append value of cost BTC

---
+++ another_visual.py
@@ -0,0 +1,8 @@
+import matplotlib.pyplot as plt
+
+plt.title ('Graphic of cost Bitcoin', fontsize=13, fontweight='bold', color = 'blue')
+plt.xlabel ('Period', fontsize=10, color='black')
+plt.ylabel ('Cost', fontsize=10, color='black')
+plt.legend()
+plt.grid()
+plt.show()
